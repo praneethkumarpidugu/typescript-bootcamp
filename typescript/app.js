65a3adfa3a7422b920ffc6bc55eb6b49cb95a094
praneethkumarpidugu <pidugu2007@gmail.com>
2018-12-18 23:55:45 -0500

    Tuples and Enums

--- typescript/app.js
+++ typescript/app.js
@@ -16,3 +16,23 @@ var myArray = ["cooking", "guitar"];
 console.log(typeof myArray);
 //  Not Good => myArray = [1];
 myArray = ["This is a string"];
+// Tuples
+var myTuples = ["hello", 99];
+// Enum
+var myColors;
+(function (myColors) {
+    myColors[myColors["Yellow"] = 0] = "Yellow";
+    myColors[myColors["Green"] = 1] = "Green";
+    myColors[myColors["Blue"] = 2] = "Blue";
+})(myColors || (myColors = {}));
+var myEnum = myColors.Yellow;
+console.log("myEnum " + myEnum);
+// Lets see how Enums affect the order.
+var myNumbers;
+(function (myNumbers) {
+    myNumbers[myNumbers["order1"] = 0] = "order1";
+    myNumbers[myNumbers["order2"] = 100] = "order2";
+    myNumbers[myNumbers["order3"] = 101] = "order3";
+})(myNumbers || (myNumbers = {}));
+var myResult = myNumbers.order3;
+console.log(myResult);
--- typescript/app.ts
+++ typescript/app.ts
@@ -22,3 +22,31 @@ console.log(typeof myArray);
 //  Not Good => myArray = [1];
 
 myArray = ["This is a string"];
+
+// Tuples
+let myTuples: [string, number] = ["hello", 99];
+
+// Enum
+
+enum myColors {
+    Yellow,
+    Green,
+    Blue
+}
+
+let myEnum: myColors = myColors.Yellow;
+console.log("myEnum " + myEnum);
+
+// Lets see how Enums affect the order.
+
+enum myNumbers {
+    order1,
+    order2 = 100,
+    order3
+}
+
+let myResult: myNumbers = myNumbers.order3;
+console.log(myResult);
+
+
+
